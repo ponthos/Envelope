999fa6c9379b228e263a6072ed401e5b3c0c534f
ponthos <1781929720@qq.com>
2019-04-12 18:05:32 +0800

    Initial commit

---
+++ app/src/main/java/com/jiayuan/jr/envelope/UI/Activity/UserActivity.kt
@@ -0,0 +1,13 @@
+package com.jiayuan.jr.envelope.UI.Activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.jiayuan.jr.envelope.R
+
+class UserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
